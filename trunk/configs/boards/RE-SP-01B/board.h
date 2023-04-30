5012f3366830dd6e2f6a18ac9ba077cb639501d5
padavanonly <meisreallyba@gmail.com>
2023-04-30 22:22:50 +0800

    add jdc cloud-1 and luban support

---
+++ trunk/configs/boards/RE-SP-01B/board.h
@@ -0,0 +1,19 @@
+ #define BOARD_PID "RE-SP-01B"
+ #define BOARD_NAME "RE-SP-01B"
+ #define BOARD_DESC "JDCloud RE-SP-01B Wireless Router"
+ #define BOARD_VENDOR_NAME "Jingdong Technology Holding Co. Ltd."
+ #define BOARD_VENDOR_URL "https://jdbox.jdcloud.com/"
+ #define BOARD_MODEL_URL "https://jdbox.jdcloud.com/"
+ #define BOARD_BOOT_TIME		30
+ #define BOARD_FLASH_TIME	120
+
+ #define BOARD_HAS_5G_11AC		1
+ #define BOARD_NUM_ANT_5G_TX		4
+ #define BOARD_NUM_ANT_5G_RX		4
+ #define BOARD_NUM_ANT_2G_TX		2
+ #define BOARD_NUM_ANT_2G_RX		2
+ #define BOARD_NUM_ETH_LEDS		1
+ #define BOARD_NUM_ETH_EPHY		4
+ #define BOARD_NUM_UPHY_USB3	1
+ #define BOARD_HAS_EPHY_L1000	1
+ #define BOARD_HAS_EPHY_W1000	1 
--- trunk/user/rc/ralink.c
+++ trunk/user/rc/ralink.c
@@ -162,7 +162,7 @@ static const struct cc_t {
 inline int
 get_wired_mac_is_single(void)
 {
-#if defined (BOARD_N14U) || defined (BOARD_N11P) || defined (BOARD_MZ_R13) || defined (BOARD_MZ_R13P) || defined (BOARD_CR660x) || defined (BOARD_Q20) || defined (BOARD_RM2100)
+#if defined (BOARD_N14U) || defined (BOARD_N11P) || defined (BOARD_MZ_R13) || defined (BOARD_MZ_R13P) || defined (BOARD_CR660x) || defined (BOARD_Q20) || defined (BOARD_RM2100) || defined (BOARD_RE_CP_02)
 	return 1;
 #else
 	return 0;
@@ -178,7 +178,7 @@ get_wired_mac_e2p_offset(int is_wan)
 	return 0xe000;
 #elif defined (BOARD_RM2100) || defined (BOARD_NEWIFI) 
 	return 0xe006;	
-#elif defined (BOARD_CR660x)  || defined (BOARD_Q20)  
+#elif defined (BOARD_CR660x)  || defined (BOARD_Q20)  || defined (BOARD_RE_CP_02)
 	return 0x3FFFA;
 #else
 	return (is_wan) ? OFFSET_MAC_GMAC2 : OFFSET_MAC_GMAC0;
