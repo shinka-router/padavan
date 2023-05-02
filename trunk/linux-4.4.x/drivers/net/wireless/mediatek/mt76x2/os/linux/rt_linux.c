aeab9adc744d7e661872e2647e32e5f2d5b8fa42
padavanonly <meisreallyba@gmail.com>
2023-05-02 17:05:59 +0800

    fix mt76x2 dat read

--- trunk/linux-4.4.x/drivers/net/wireless/mediatek/mt76x2/os/linux/rt_linux.c
+++ trunk/linux-4.4.x/drivers/net/wireless/mediatek/mt76x2/os/linux/rt_linux.c
@@ -877,13 +877,19 @@ void RtmpOSFileSeek(RTMP_OS_FD osfd, int offset)
 int RtmpOSFileRead(RTMP_OS_FD osfd, char *pDataPtr, int readLen)
 {
 	/* The object must have a read method */
+#if (KERNEL_VERSION(3, 19, 0) > LINUX_VERSION_CODE)
 	if (osfd->f_op && osfd->f_op->read) {
 		return osfd->f_op->read(osfd, pDataPtr, readLen, &osfd->f_pos);
+#else
+
+	if (osfd->f_mode & FMODE_CAN_READ) {
+		return __vfs_read(osfd, pDataPtr, readLen, &osfd->f_pos);
+#endif
 	} else {
-		DBGPRINT(RT_DEBUG_ERROR, ("no file read method\n"));
 		return -1;
 	}
 }
+
 
 
 int RtmpOSFileWrite(RTMP_OS_FD osfd, char *pDataPtr, int writeLen)
